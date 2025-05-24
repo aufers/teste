822b2cfb7a4e12840570898ff11fde5516f3a9e3
aufers <aufers2013@gmail.com>
2025-05-24 10:54:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Meu Site</title>
+</head>
+
+<body>
+<img src="imagem/goo.jpg" width="120" height="60" alt="Google"/>
+</body>
+</html>
